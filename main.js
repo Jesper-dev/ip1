1b5ab198860008b461bb0c82c37d6a3c12cbfdb9
JesperKYH <jesper.pettersson@student.kyh.se>
2020-09-28 16:22:46 +0200

    Added comments in main.js

--- main.js
+++ main.js
@@ -4,14 +4,11 @@ let link = document.querySelectorAll(".link");
 
 for(let i = 0; i < link.length; i++){
 
+        //Checks if the current link has a class of "active"
+        //If it does it removes the link class and adds the current.link class instaed.
         if(link[i].classList.contains("active")){
             link[i].classList.remove("link");
             link[i].classList.add("current-link")
-        }else{
-            link[i].classList.remove("current-link");
-        }
-
-        console.log(link[i]);
+        }       
 }
 
-console.log(link);
