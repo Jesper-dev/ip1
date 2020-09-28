49d8f523c10b92d0d06448a6acdfe8ae73914f78
JesperKYH <jesper.pettersson@student.kyh.se>
2020-09-28 16:14:52 +0200

    Added so links show what page you are on

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ let link = document.querySelectorAll(".link");
 
 for(let i = 0; i < link.length; i++){
 
-        if(link[i].classList.contains(".active")){
-
-            link[i].classList.add("current-link");
+        if(link[i].classList.contains("active")){
+            link[i].classList.remove("link");
+            link[i].classList.add("current-link")
         }else{
             link[i].classList.remove("current-link");
         }
+
+        console.log(link[i]);
 }
 
 console.log(link);
