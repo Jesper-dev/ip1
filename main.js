9f65f8cd04be631a4a906ed28ec3edff7a01a072
JesperKYH <jesper.pettersson@student.kyh.se>
2020-10-12 16:01:20 +0200

    Small changes

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 //Link Stuff
 let link = document.querySelectorAll(".link");
 
+for (let i = 0; i < link.length; i++) {
 
-for(let i = 0; i < link.length; i++){
-
-        //Checks if the current link has a class of "active"
-        //If it does it removes the link class and adds the current.link class instaed.
-        if(link[i].classList.contains("active")){
-            link[i].classList.remove("link");
-            link[i].classList.add("current-link")
-        }       
-}
-
+    //Checks if the current link has a class of "active"
+    //If it does it removes the link class and adds the current.link class instaed.
+    if (link[i].classList.contains("active")) {
+        link[i].classList.remove("link");
+        link[i].classList.add("current-link")
+    }
+};
